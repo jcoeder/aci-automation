6d052174ba29b176146e57e8e042795562336580
Justin Oeder <jcoeder@gmail.com>
2019-12-18 18:27:40 -0500

    Update provision-switch-serial.py

--- provision-switch-serial.py
+++ provision-switch-serial.py
@@ -1,5 +1,7 @@
 # Update:
 # ser.port
+# gateway and subnet
+# list of IP addresses - 
 # Use this to walk around the data center after rack and stack
 # to set IP addresses to upload ACI Image
 # About 40 seconds per switch, prevents human fat finger errors
@@ -7,6 +9,9 @@
 import getpass
 import serial
 import time
+
+gateway = "192.168.254.1"
+subnet =  "/24"
 
 #initialization and open the port
 
@@ -40,9 +45,6 @@ ser.writeTimeout = 2     #timeout for write
 
 with open("oob-addresses.txt") as f1:
     oob_addresses = f1.read().splitlines()
-
-with open("oob-network.txt") as f2:
-    oob_network = f2.read().splitlines()
 
 ser.open()
 
@@ -93,7 +95,7 @@ ser.write(b'interface mgmt 0\n')
 time.sleep(1)
 ser.write(b'no shutdown\n')
 time.sleep(1)
-mgmt_ip = bytes('ip address ' + oob_addresses[0] + oob_network[0] + '\n', 'utf-8')
+mgmt_ip = bytes('ip address ' + oob_addresses[0] + subnet + '\n', 'utf-8')
 ser.write(mgmt_ip)
 time.sleep(1)
 ser.write(b'exit\n')
@@ -102,7 +104,7 @@ time.sleep(1)
 # Configure default route
 ser.write(b'vrf context management\n')
 time.sleep(1)
-def_route = bytes('ip route 0.0.0.0/0 ' + oob_network[1] + ' vrf management\n', 'utf-8')
+def_route = bytes('ip route 0.0.0.0/0 ' + gateway + ' vrf management\n', 'utf-8')
 ser.write(def_route)
 time.sleep(1)
 
